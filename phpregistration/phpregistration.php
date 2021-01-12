08a48f29fda217f1c9f8f6b0090284d3026090da
Iftekh <i.tekh20@yandex.com>
2021-01-12 17:16:50 +0600

    added php registration functionality

---
+++ phpregistration/phpregistration.php
@@ -0,0 +1,22 @@
+function register_user($username, $email, $password){
+
+global $connection;
+
+    $username = mysqli_real_escape_string($connection, $username);
+    $email    = mysqli_real_escape_string($connection, $email);
+    $password = mysqli_real_escape_string($connection, $password);
+
+    $password = password_hash( $password, PASSWORD_BCRYPT, array('cost' => 12));
+        
+        
+    $query = "INSERT INTO users (username, user_email, user_password, user_role) ";
+    $query .= "VALUES('{$username}','{$email}', '{$password}', 'subscriber' )";
+    $register_user_query = mysqli_query($connection, $query);
+
+    confirmQuery($register_user_query);
+
+    
+
+
+
+}
